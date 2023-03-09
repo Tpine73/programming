295f12af8a2f4945d6af05835093ef8fe98467fa
shyProgrammer351 <123141646+shyProgrammer351@users.noreply.github.com>
2023-03-09 18:20:32 +0900

    Add files via upload

--- C/Chapter01/Heart.c
+++ C/Chapter01/Heart.c
@@ -4,11 +4,11 @@
 int main() 						
 {
 	// 하트 모양을 출력 
-	printf( "\033[0;31m .d88b.d88b. \n" ); 	
-	printf( " 88888888888 \n" );  	
-	printf( " `Y8888888Y' \n" );  	
-	printf( "  `Y888Y' \n" ); 	
-	printf( "    `Y' \n" );  	
+	printf( " \033[31m .d88b.d88b. \n" ); 	
+	printf( "  88888888888 \n" );  	
+	printf( "  `Y8888888Y' \n" );  	
+	printf( "   `Y888Y' \n" ); 	
+	printf( "     `Y' \n" );  	
 
 	// 프로그램 종료 
 	return 0;
--- C/Chapter01/RollingDice.c
+++ C/Chapter01/RollingDice.c
@@ -5,57 +5,57 @@
 int main()
 {
 	// 1번 주사위 출력  
-	printf("\033[H\033[J");
-	printf("+-------+\n");
-	printf("|       |\n");
-	printf("|   O   |\n");
-	printf("|       |\n");
-	printf("+-------+\n");
+	printf(" \033[H\033[J");
+	printf(" +-------+ \n");
+	printf(" |       | \n");
+	printf(" |   O   | \n");
+	printf(" |       | \n");
+	printf(" +-------+ \n");
 	
 	// 잠시 후 2번 주사위 출력  
 	sleep(2);
-	printf("\033[H\033[J");
-	printf("+-------+\n");
-	printf("| O     |\n");
-	printf("|       |\n");
-	printf("|     O |\n");
-	printf("+-------+\n");
+	printf(" \033[H\033[J");
+	printf(" +-------+ \n");
+	printf(" | O     | \n");
+	printf(" |       | \n");
+	printf(" |     O | \n");
+	printf(" +-------+ \n");
 	
 	// 잠시 후 3번 주사위 출력  
 	sleep(2);
-	printf("\033[H\033[J");
-	printf("+-------+\n");
-	printf("| O     |\n");
-	printf("|   O   |\n");
-	printf("|     O |\n");
-	printf("+-------+\n");
+	printf(" \033[H\033[J");
+	printf(" +-------+ \n");
+	printf(" | O     | \n");
+	printf(" |   O   | \n");
+	printf(" |     O | \n");
+	printf(" +-------+ \n");
 	
 	// 잠시 후 4번 주사위 출력  
 	sleep(2);
-	printf("\033[H\033[J");
-	printf("+-------+\n");
-	printf("| O   O |\n");
-	printf("|       |\n");
-	printf("| O   O |\n");
-	printf("+-------+\n");
+	printf(" \033[H\033[J");
+	printf(" +-------+ \n");
+	printf(" | O   O | \n");
+	printf(" |       | \n");
+	printf(" | O   O | \n");
+	printf(" +-------+ \n");
 	
 	// 잠시 후 5번 주사위 출력  
 	sleep(2);
-	printf("\033[H\033[J");
-	printf("+-------+\n");
-	printf("| O   O |\n");
-	printf("|   O   |\n");
-	printf("| O   O |\n");
-	printf("+-------+\n");
+	printf(" \033[H\033[J");
+	printf(" +-------+ \n");
+	printf(" | O   O | \n");
+	printf(" |   O   | \n");
+	printf(" | O   O | \n");
+	printf(" +-------+ \n");
 	
 	// 잠시 후 6번 주사위 출력  
 	sleep(2);
-	printf("\033[H\033[J");
-	printf("+-------+\n");
-	printf("| O   O |\n");
-	printf("| O   O |\n");
-	printf("| O   O |\n");
-	printf("+-------+\n");
+	printf(" \033[H\033[J");
+	printf(" +-------+ \n");
+	printf(" | O   O | \n");
+	printf(" | O   O | \n");
+	printf(" | O   O | \n");
+	printf(" +-------+ \n");
 	
 	// 프로그램 종료 
 	return 0;
