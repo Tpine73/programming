cdb69e145ae7e8c7bf0e9d713021fc891b3bea8d
shyProgrammer351 <123141646+shyProgrammer351@users.noreply.github.com>
2023-04-07 19:11:59 +0900

    Update SQLconstants.php

--- web/book/php/SQLconstants.php
+++ web/book/php/SQLconstants.php
@@ -1,9 +1,9 @@
 <?php 
-	// MySQL ID : ex) teamA, teamB, teamC ...
-	$mySQL_id = "teamA"; 	
+	// MySQL ID : MySQL 설치시 아이디 ex) root, test, ...
+	$mySQL_id = "test"; 	
 
 	// MySQL Password	
-	$mySQL_password = "HAGF9221";
+	$mySQL_password = "test";
 
 	// MySQL Server 
 	$mySQL_host = "localhost";
